ac8306449ee5eef6b8a3bafd7aa3a670537667a6
Tricia Swift <tricia.swift8@gmail.com>
2023-07-26 14:07:54 -0500

    fired up the kiln

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,19 @@
 // Imports go first
 import { makePottery } from "./PotteryWheel.js";
+import { firePottery } from "./Kiln.js";
 
 // Make 5 pieces of pottery at the wheel
-makePottery("Bowl", 2, 5);
-makePottery("Mug", 1, 4);
-makePottery("Plate", 3, 1);
-makePottery("Sculpture", 5, 8);
-makePottery("Vase", 4, 10);
+const bowl = makePottery("Bowl", 2, 5);
+const mug = makePottery("Mug", 1, 4);
+const plate = makePottery("Plate", 3, 1);
+const sculpture = makePottery("Sculpture", 5, 8);
+const vase = makePottery("Vase", 4, 10);
 // Fire each piece of pottery in the kiln
-
+const fireBowl = firePottery(bowl, 2000);
+const fireMug = firePottery(mug, 2200);
+const firePlate = firePottery(plate, 2500);
+const fireSculpture = firePottery(sculpture, 2050);
+const fireVase = firePottery(vase, 1800);
 // Determine which ones should be sold, and their price
 
 // Invoke the component function that renders the HTML list
